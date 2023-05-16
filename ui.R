b34e5ef82473571299f25779cdd338f0413eb869
charliejhadley <charliejhadley@gmail.com>
2023-05-16 17:54:13 +0100

    just after talk - before questions

---
+++ ui.R
@@ -0,0 +1,9 @@
+fluidPage(
+  "Hello world",
+  sliderInput("exponent",
+              label = "Choose an exponent",
+              min = 1,
+              max = 6,
+              value = 2),
+  plotOutput("curve_plot")
+)
